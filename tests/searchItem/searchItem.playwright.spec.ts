0acd604fecc499f2324d95f31bafdc6b0c9cd5f8
Aljtas <otaman.19.20.70@gmail.com>
2024-10-11 18:17:56 +0300

    update ui+api test

--- tests/searchItem/searchItem.playwright.spec.ts
+++ tests/searchItem/searchItem.playwright.spec.ts
@@ -12,10 +12,10 @@ interface ApiResponse {
   
 test.describe('Playwright: search functionality of OLX', () => {
     const searchString: string = 'iPhone';
-    const getOffersPath = 'api/v1/offers/?offset&query='
-
+ 
     test('search item in the system: api', async ({request, page}) => { 
         const url = new BasePage(page);
+        const getOffersPath = 'api/v1/offers/?offset&query='
         const response = await getApiRequestWithApi<ApiResponse>( request, `${url.pageUrl}${getOffersPath}${searchString}`);
         const listOfTitles = response.data.map(item =>item.title);
         expect(everyItemsIncludes(listOfTitles, searchString)).toBeTruthy();
@@ -33,13 +33,18 @@ test.describe('Playwright: search functionality of OLX', () => {
     test('search item in the system: api and ui', async ({ request, page }) => {
         const url = new BasePage(page);
         const mainPage = new MainPage(page);
-        
-        const response = await getApiRequestWithApi<ApiResponse>( request, `${url.pageUrl}${getOffersPath}${searchString}`);
+        const listOfTitlesApi: string[] = [];
+
+        for(let i=0; i<=1000; i=i+40) {
+            const getOffersPath = `api/v1/offers?offset=${i}&query=`;
+            const response = await getApiRequestWithApi<ApiResponse>( request, `${url.pageUrl}${getOffersPath}${searchString}`);
+            listOfTitlesApi.push(...response.data.map(item =>item.title));
+        }
+
         await mainPage.navigate();
         await validateElement(page, '[data-testid="search-input"]', element => element.fill(searchString));
         await validateElement(page, '[data-testid="search-submit"]', element => element.click());
         
-        const listOfTitlesApi = response.data.map(item =>item.title);
         const listOfTitlesUi = await validateElement(page, '[data-testid="l-card"] h6', element => element.innerText());
         for(let index = 0; index < 2; index++){
             expect(listOfTitlesApi).toContain(listOfTitlesUi[index]);
